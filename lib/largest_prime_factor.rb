354f5a5d2ed45317c48a369c6183f4a161a2f005
SkaMateria <skamateria@gmail.com>
2019-11-05 10:43:25 -0500

    apparently i should have googled what a prime factor was... go me

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,14 @@
-# Enter your procedural solution here!
+# allNum = [*1..600851475143]
+
+allNum = [*1..40]
+
+noEvens = allNum.select{ |x| x%2 != 0 && x < 1 }
+
+newArray = noEvens.insert(1, 2)
+
+def is_prime?(num)
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
+end
+
+newArray.each{|x| }
